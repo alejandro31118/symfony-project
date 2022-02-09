34bdb83541c951ba781ab67a569aeb4c938a2258
Alejandro Perez <alejandro31118@gmail.com>
2022-02-09 13:15:14 +0000

    Add toString method

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -34,6 +34,13 @@ class Game
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
--- src/Entity/Platform.php
+++ src/Entity/Platform.php
@@ -38,6 +38,13 @@ class Platform
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
