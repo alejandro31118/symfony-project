43a58148ddc9663f7cbe821ba816257003277e9c
Alejandro Perez <alejandro31118@gmail.com>
2022-02-09 13:16:08 +0000

    Add toString method

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -88,4 +88,9 @@ class Game
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
--- src/Entity/Platform.php
+++ src/Entity/Platform.php
@@ -107,4 +107,9 @@ class Platform
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
